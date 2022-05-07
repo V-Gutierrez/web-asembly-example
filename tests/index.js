778225407a1b8efd91c55622283c6c6f03c49643
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-05-07 12:00:57 -0300

    Chore: implement an AS function

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,8 @@
-import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
-console.log("ok");
+import assert from "assert"
+import { add, minusOne } from "../build/debug.js"
+
+assert.strictEqual(add(1, 2), 3)
+assert.strictEqual(minusOne(1), 0)
+
+
+console.log("Tests passed")
