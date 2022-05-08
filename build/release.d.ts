66b4375542bf7b8c606829cbe1e4d55d7e665c5c
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-05-08 11:26:46 -0300

    commit builds

---
+++ build/release.d.ts
@@ -0,0 +1,19 @@
+/**
+ * assembly/index/add
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function add(a: number, b: number): number;
+/**
+ * assembly/index/minusOne
+ * @param n `i32`
+ * @returns `i32`
+ */
+export declare function minusOne(n: number): number;
+/**
+ * assembly/index/fizzBuzz
+ * @param n `i32`
+ * @returns `~lib/string/String | null`
+ */
+export declare function fizzBuzz(n: number): string | null;
